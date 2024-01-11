4880f9dfc60b55137adc1a79303725479cb256d7
Edilaine Alves dos Santos <91744159+edilaine-as@users.noreply.github.com>
2024-01-11 14:35:43 -0300

    Add OrderSucess

--- src/pages/OrderSucess/index.tsx
+++ src/pages/OrderSucess/index.tsx
@@ -1,3 +1,55 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+import imgOrderSucess from '../../../public/img-order-sucess.svg'
+import { Info, Order, Container, Heading, InfoContent } from './styles'
+
 export function OrderSucess() {
-  return <h1>Order Sucess</h1>
+  return (
+    <Container>
+      <Order>
+        <Heading>
+          <h1>Uhu! Pedido confirmado</h1>
+          <p>Agora é só aguardar que logo o café chegará até você</p>
+        </Heading>
+        <Info>
+          <InfoContent>
+            <div>
+              <MapPin/>
+              <div>
+                <span>Entrega em 
+                  <strong> Rua João Daniel Martinelli, 102</strong>
+                </span>
+                <span>
+                  Farrapos - Porto Alegre, RS
+                </span>
+              </div>
+            </div>
+
+            <div>
+              <Timer/>
+              <div>
+                <span>Previsão de entrega
+                </span>
+                <span>
+                  <strong>20 min - 30 min</strong>
+                </span>
+              </div>
+            </div>
+
+            <div>
+              <CurrencyDollar/>
+              <div>
+                <span>Pagamento na entrega
+                </span>
+                <span>
+                  <strong>Cartão de crédito</strong>
+                </span>
+              </div>
+            </div>
+
+          </InfoContent>
+        </Info>
+      </Order>
+      <img src={imgOrderSucess} />
+    </Container>
+  )
 }
---
+++ src/pages/OrderSucess/styles.ts
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { mixins } from "../../styles/mixins";
+
+export const Container = styled.main`
+    display: flex;
+    justify-content: space-between;
+    margin: 0 auto;
+    margin-top: 5rem;
+    max-width: 1160px;
+`
+
+export const Order = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    min-width: 526px;
+`
+
+export const Heading = styled.div`
+    h1{
+        ${mixins.fonts.titleL};
+        color: ${(props) => props.theme["yellow-dark"]};
+    }
+    p{
+        ${mixins.fonts.textL};
+        color: ${(props) => props.theme["base-subtitle"]};
+    }
+`
+
+export const Info = styled.div`
+    width: 100%;
+
+    border: 1px solid;
+    border-color: transparent;
+    border-radius: 6px 36px;
+    border-color: transparent;
+
+    background-origin: border-box;
+    background-image: linear-gradient(to bottom right, ${(props) => props.theme.yellow}, ${(props) => props.theme.purple});
+`
+
+export const InfoContent = styled.div`
+    padding: 40px;
+    background-color: white;
+    border-radius: 6px 36px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+
+    > div{
+        display: flex;
+        gap: 12px;
+
+        > svg {
+            padding: 8px;
+            border-radius: 999px;
+          }
+
+        > div{
+            display: flex;
+            flex-direction: column;
+        }
+    }
+`
